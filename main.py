c4bac3cd85c27a62b76f060b98a525fe2cf4ec0b
jhujhufragoso-png <jhujhufragoso@gmail.com>
2026-06-12 17:44:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
- n1 = float( input('Digite um número') )
- n2 = float( input('Digite um número') )
+n1 = float( input('Digite um número') )
+n2 = float( input('Digite um número') )
 
- soma  =  n1 + n2 print(soma)
+soma  =  n1 + n2 
+print(soma)
 
- n1 = float( input('Digite um número') )
- n2 = float( input('Digite um número') )
+n1 = float( input('Digite um número') )
+n2 = float( input('Digite um número') )
 
- subtração= n1 - n2 print(subtração)
+subtração= n1 - n2 
+print(subtração)
+
+n1 = float( input('Digite um número') )
+n2 = float( input('Digite um número') )
+
+multiplicação= n1 - n2 
+print(multiplicação)
+
+n1 = float( input('Digite um número') )
+n2 = float( input('Digite um número') )
+
+divisão= n1 - n2 
+print(divisão)
 
