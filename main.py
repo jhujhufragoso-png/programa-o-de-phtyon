a21685a270d33238e5ac57a76eb495e37a2a3c6e
jhujhufragoso-png <jhujhufragoso@gmail.com>
2026-06-26 16:30:17 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,88 +1,26 @@
+def valor_hora(carga, salario):
+    return salario / carga
 
-a = int(input())
-b = int(input())
+def extra_50(valor_hora):
+    return valor_hora * 1.5
 
-try:
-      print(a/b)
-
-except ZeroDivisionError as error:
-      print(error)
-
-else:
-      print( ' Sem erros')
-
-finally:
-      print('Aqui sempre vai printar')
+def salario_total(quantidade_extra, salario, extra_50_):
+    valor_q = quantidade_extra * extra_50_
+    total   =  valor_q + salario
+    return total, valor_q
 
 
-----------------------------------------------------
-
-
-def trantando_erros():
-    try: 
-        numero = int(input('>>'))
-    except ZeroDivisionError as erro:
-        print(erro)
-    except ValueError as erro2:
-        print('Erro no valor da variável')  
-    except NameError as erro:
-        print(erro)      
-    finally:
-        print('O sistema foi carregado')
-
-trantando_erros()
-
-
----------------------------------------------------------
-
-
-def divisao():
-
-    try:
-      n1 = float(input('>>'))
-      n2 = float(input('>>'))
-      div = n1/n2
-      print(div)  
-    except ZeroDivisionError:
-        print("A divisão não ser por zero")
-    except ValueError:
-        print('Um texo foi digitado')
-    except TypeError as erro:
-        print(erro)
-
-    else:
-        print('Algo deu errado ')    
-
-     
-           
-divisao() 
-
-
-
-
-import statistics 
-
-
-def divisao():
-    try:
-     lista=[0,2,1 , 5]
-     moda  =  statistics.mode(lista)
-     print(moda)
-    except ZeroDivisionError as erro:
-        print(erro)
-    except TypeError:
-        print('Provavelmente digitaram letras')
-    except ValueError as erro :
-        print(erro)
-    except NameError as erro:
-        print(erro)    
-    except SyntaxError as erro:
-        print(erro)     
-    finally:
-        print('Carregamento finalizado')
-
-
-divisao()        
-
-
-  
+def sistema_RH():
+    carga  = float(input('Carga: '))
+    salario = float(input('Salário: '))
+    v_h  = valor_hora(carga, salario)
+    print(f'Valor hora: , {v_h:.2f}')
+    print('------------- ')
+    print('HORA EXTRA 50% ')
+    ex_50 = extra_50(v_h)
+    print(f'{ex_50:.2f}')
+    print('------------- ')
+    quantidade = float(input('Extra quantidade: '))
+    sal = salario_total(quantidade, salario, ex_50 )
+    print('R$', round(sal[0],2), "Total hora extra",  round(sal[1],2))
+sistema_RH()
